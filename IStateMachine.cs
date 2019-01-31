b6aa848d03b7995728e2b0653cbaf6facb33bde9
Simon Jefferies <simon@olivegame.studio>
2019-01-31 21:59:33 +0000

    Added source code.

---
+++ IStateMachine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OliveGameStudio.Core.StateMachine
+{
+    public interface IStateMachine<T> where T : Enum
+    {
+        T CurrentState { get; }
+
+        TimeSpan TimeInState { get; }
+
+        void AddUpdate(T state, Action<TimeSpan> action);
+
+        void ChangeState(T state);
+
+        event Action<T> Changed;
+
+        void Update(TimeSpan time);
+
+    }
+}
---
+++ SimpleStateMachine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OliveGameStudio.Core.StateMachine
+{
+    public class SimpleStateMachine<T> : IStateMachine<T> where T : Enum
+    {
+        public T CurrentState { get; private set; }
+
+        public TimeSpan TimeInState { get; private set; }
+
+        readonly Dictionary<T, Action<TimeSpan>> _stateUpdate = new Dictionary<T,Action<TimeSpan>>();
+
+        public SimpleStateMachine(T initialState)
+        {
+            CurrentState = initialState;
+        }
+
+        public void AddUpdate(T state, Action<TimeSpan> action)
+        {
+            _stateUpdate.Add(state, action);
+        }
+
+        public void ChangeState(T state)
+        {
+            CurrentState = state;
+            TimeInState = TimeSpan.Zero;
+            Changed.Invoke(CurrentState);
+        }
+
+        public event Action<T> Changed = delegate { };
+
+        public void Update(TimeSpan time)
+        {
+            TimeInState += time;
+            if (_stateUpdate.ContainsKey(CurrentState))
+            {
+                _stateUpdate[CurrentState].Invoke(time);
+            }
+        }
+    }
+}
